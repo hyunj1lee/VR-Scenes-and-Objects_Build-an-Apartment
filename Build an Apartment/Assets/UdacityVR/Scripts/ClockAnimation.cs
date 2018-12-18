75bbfc4a380143438ea9e1c9a8d2b20ca0e962b1
hyun ji lee <hyun_.lee@outlook.com>
2018-12-18 07:50:30 -0200

    add clock animation script

---
+++ Build an Apartment/Assets/UdacityVR/Scripts/ClockAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockAnimation : MonoBehaviour {
+	public Transform SecondHand;
+	public Transform MinuteHand;
+	public Transform HourHand;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime currentTime = DateTime.Now;
+		float second = (float) currentTime.Second;
+		float minute = (float)currentTime.Minute;
+		float hour = (float)currentTime.Hour %12;
+
+		float secondAngle = 360 * (second/60);
+		float minuteAngle = 360 * (minute/60);
+		float hourAngle = 360 * (hour/12);
+
+		SecondHand.localRotation = Quaternion.Euler (0, 0, secondAngle);
+		MinuteHand.localRotation = Quaternion.Euler (0, 0, minuteAngle);
+		HourHand.localRotation = Quaternion.Euler (0, 0, hourAngle);
+	}
+}
